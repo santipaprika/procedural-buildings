de605da81b36879bd0ec6935fff097ee7474fc53
Guillem Perez Delgado <guillem.perez.delgado@gmail.com>
2021-05-14 21:49:26 +0200

    Implement first draft of Scope class

---
+++ Scope.cpp
@@ -0,0 +1,57 @@
+#include "Scope.h"
+
+#include "glm/ext.hpp"
+
+Scope::Scope()
+    : position(0.0f)
+    , size(1.0f)
+    , x(1.0f, 0.0f, 0.0f)
+    , y (0.0f, 1.0f, 0.0f)
+    , z (0.0f, 0.0f, 1.0f)
+    {}
+
+Scope::Scope(const Scope &other)
+    : position(other.position)
+    , size(other.size)
+    , x(other.x)
+    , y(other.y)
+    , z(other.z)
+    {}
+
+void Scope::T(const glm::vec3 &translation)
+{
+    position += translation;
+}
+
+void Scope::S(const glm::vec3 &size_)
+{
+    size += size_;
+}
+
+void Scope::Rx(float angle)
+{
+    float c = glm::cos(angle);
+    float s = glm::sin(angle);
+    glm::mat3 rotation(glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, c, s), glm::vec3(0.0f, -s, c));
+    y = rotation * y;
+    z = rotation * z;
+
+}
+
+void Scope::Ry(float angle)
+{
+    float c = glm::cos(angle);
+    float s = glm::sin(angle);
+    glm::mat3 rotation(glm::vec3(c, 0.0f, -s), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(s, 0.0f, c));
+    x = rotation * x;
+    z = rotation * z;
+}
+
+void Scope::Rz(float angle)
+{
+    float c = glm::cos(angle);
+    float s = glm::sin(angle);
+    glm::mat3 rotation(glm::vec3(c, s, 0.0f), glm::vec3(-s, c, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    x = rotation * x;
+    y = rotation * y;
+}
---
+++ Scope.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "glm/glm.hpp"
+
+class Scope
+{
+public:
+    Scope();
+    Scope(const Scope &other);
+    void T(const glm::vec3 &translation);
+    void S(const glm::vec3 &size);
+    void Rx(float angle);
+    void Ry(float angle);
+    void Rz(float angle);
+private:
+    glm::vec3 position;
+    glm::vec3 size;
+    glm::vec3 x, y, z;
+};
