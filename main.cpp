3a12b651af3b20af1b3f5df46e78130bc9276696
santipaprika <santi.gj4@gmail.com>
2021-05-14 20:53:11 +0200

    Include GLM external library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "glm/glm.hpp"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    glm::vec3 a(0.f);
+    std::cout << glm::to_string(a) << std::endl;
     return 0;
 }
