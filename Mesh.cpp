db32e09d2dfe0c93587fcecae88d6b3e0b5bc2ea
Guillem Perez Delgado <guillem.perez.delgado@gmail.com>
2021-06-17 13:04:19 +0200

    Add spheres as primitives

--- Mesh.cpp
+++ Mesh.cpp
@@ -29,10 +29,25 @@ void Mesh::add(const Mesh &other)
         faces.emplace_back(face + glm::ivec3(numVertices));
 }
 
+void Mesh::addTriangle(int i0, int i1, int i2)
+{
+    faces.emplace_back(i0, i1, i2);
+}
+
+void Mesh::addQuad(int i0, int i1, int i2, int i3)
+{
+    addTriangle(i0, i1, i2);
+    addTriangle(i0, i2, i3);
+}
+
 Mesh Mesh::getInstance(const std::string &objId)
 {
     if (objId == "Cylinder") {
         return getCylinder();
+    }
+
+    if (objId == "Sphere") {
+        return getSphere();
     }
 
     else if (objId != "Cube") {
@@ -47,24 +62,20 @@ Mesh Mesh::getCube()
 {
     Mesh cube;
 
-    // cube.vertices = {{1, 0, 1}, {1, 1, 1}, {0, 0, 1}, {0, 1, 1},
-    //                      {1, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 0}};
-
     cube.vertices = {{0.5, 0, 0.5}, {0.5, 1, 0.5}, {-0.5, 0, 0.5}, {-0.5, 1, 0.5},
-                         {0.5, 0, -0.5}, {0.5, 1, -0.5}, {-0.5, 0, -0.5}, {-0.5, 1, -0.5}};
+                    {0.5, 0, -0.5}, {0.5, 1, -0.5}, {-0.5, 0, -0.5}, {-0.5, 1, -0.5}};
 
-    cube.faces = {{0, 3, 2}, {0, 1, 3}, // TOP
-                      {5, 4, 7}, {4, 6, 7}, // BOTTOM
-                      {4, 0, 6}, {0, 2, 6}, // LEFT
-                      {5, 3, 1}, {5, 7, 3}, // RIGHT
-                      {4, 1, 0}, {5, 1, 4}, // FRONT
-                      {2, 3, 6}, {6, 3, 7}  // BACK
+    cube.faces =    {{0, 3, 2}, {0, 1, 3}, // TOP
+                    {5, 4, 7}, {4, 6, 7}, // BOTTOM
+                    {4, 0, 6}, {0, 2, 6}, // LEFT
+                    {5, 3, 1}, {5, 7, 3}, // RIGHT
+                    {4, 1, 0}, {5, 1, 4}, // FRONT
+                    {2, 3, 6}, {6, 3, 7}  // BACK
     };
 
     return cube;
 }
 
-// TODO: Test fix indices
 Mesh Mesh::getCylinder()
 {
     const int n = 40;
@@ -107,16 +118,69 @@ Mesh Mesh::getCylinder()
 
 Mesh Mesh::getSphere()
 {
-    const int n_phi = 10;
-    const int n_theta = 20;
-    float R = 0.5f;
-    glm::vec3 translation(0.5f, 0.5f, 0.5f);
+    const int n_phi = 20; // Goes from 0 to pi
+    const int n_theta = 40; // Goes from 0 to 2*pi
+    const float R = 0.5f;
+    const float pi = glm::pi<float>();
     Mesh sphere;
 
-    glm::vec3 northPole = glm::vec3(0.0f, 0.0f, R) + translation;
+    // Add north pole
+    glm::vec3 northPole = glm::vec3(0.0f, R, 0.0f);
+    sphere.vertices.push_back(northPole);
 
+    // Add layer connected to north pole
+    float t_phi = float(1)/float(n_phi);
+    float c_phi = glm::cos(pi * t_phi);
+    float s_phi = glm::sin(pi * t_phi);
 
+    for (int i = 1; i < n_theta; ++i) {
+        float t_theta = float(i)/float(n_theta);
+        float c_theta = glm::cos(2*pi * t_theta);
+        float s_theta = glm::sin(2*pi * t_theta);
 
+        glm::vec3 vertex = R * glm::vec3(s_phi * c_theta, c_phi, s_phi * s_theta);
+        sphere.vertices.push_back(vertex);
+        sphere.addTriangle(i+1, i, 0);
+    }
+    sphere.vertices.push_back(R * glm::vec3(s_phi, c_phi, 0.0f));
+    sphere.addTriangle(1, n_theta, 0);
 
+    int offset = n_theta;
+
+    // Add middle layers
+    for (int j = 2; j < n_phi; ++j) {
+        t_phi = float(j)/float(n_phi);
+        c_phi = glm::cos(pi * t_phi);
+        s_phi = glm::sin(pi * t_phi);
+        for (int i = 1; i < n_theta; ++i) {
+            float t_theta = float(i)/float(n_theta);
+            float c_theta = glm::cos(2*pi * t_theta);
+            float s_theta = glm::sin(2*pi * t_theta);
+
+            glm::vec3 vertex = R * glm::vec3(s_phi * c_theta, c_phi, s_phi * s_theta);
+            sphere.vertices.push_back(vertex);
+            sphere.addQuad(i+1 + offset, i + offset, i + offset - n_theta, i+1 + offset - n_theta);
+        }
+        sphere.vertices.push_back(R * glm::vec3(s_phi, c_phi, 0.0f));
+        sphere.addQuad(1 + offset, n_theta + offset, offset, 1 + offset - n_theta);
+        offset += n_theta;
+    }
+
+    // Add layer connected to south pole
+    for (int i = 1; i < n_theta; ++i) {
+        float t_theta = float(i)/float(n_theta);
+        float c_theta = glm::cos(2*pi * t_theta);
+        float s_theta = glm::sin(2*pi * t_theta);
+        glm::vec3 vertex = R * glm::vec3(s_phi * c_theta, c_phi, s_phi * s_theta);
+        sphere.vertices.push_back(vertex);
+        sphere.addTriangle(i+1 + offset, n_theta + 1 + offset, i + offset);
+    }
+    sphere.vertices.push_back(R * glm::vec3(s_phi, c_phi, 0.0f));
+    sphere.addTriangle(1 + offset, n_theta + 1 + offset, n_theta + offset);
+
+    // Add north pole
+    glm::vec3 southPole = glm::vec3(0.0f, -R, 0.0f);
+    sphere.vertices.push_back(southPole);
+    
     return sphere;
 }
--- Mesh.h
+++ Mesh.h
@@ -13,6 +13,8 @@ public:
 
     void add(const Mesh &other);
     void apply(const glm::mat4 &transform);
+    void addTriangle(int i0, int i1, int i2);
+    void addQuad(int i0, int i1, int i2, int i3);
     static Mesh getInstance(const std::string &objId);
 public:
     std::vector<glm::vec3> vertices;
